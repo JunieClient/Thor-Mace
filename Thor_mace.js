16a4643324659d685f36d4a58ca87cfad04fd6ee
JunieClientDev <jjmathisb@gmail.com>
2026-09-04 19:21:40 -0400

    Create Thor_mace.js

---
+++ Thor_mace.js
@@ -0,0 +1,20 @@
+{
+  "format_version": "1.20.50",
+  "minecraft:item": {
+    "description": {
+      "identifier": "thor_mace:thor_mace",
+      "category": "equipment"
+    },
+    "components": {
+      "minecraft:icon": {
+        "texture": "mace"
+      },
+      "minecraft:display_name": {
+        "value": "thor_mace:thor_mace"
+      },
+      "minecraft:hand_equipped": true,
+      "minecraft:max_stack_size": 1,
+      "minecraft:damage": 7
+    }
+  }
+}
